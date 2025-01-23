dbca9601ac102af70b60b954f8a49ed171ab7804
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-23 03:36:33 +0100

    feat: Add Filter component with input handling
    - Implemented Filter component to handle input changes
    - Added handleChange function to manage filter input events
    - Applied styling for better layout and appearance
    - Enhanced user experience with dynamic filtering input

---
+++ src/components/Filter.jsx
@@ -0,0 +1,18 @@
+const Filter = () => {
+
+    const handleChange = (event) => {
+        event.preventDefault()-
+    }
+
+    const style = {
+        marginBottom: 10,
+    };
+
+  return (
+    <div style={style}>
+      filter <input onChange={handleChange} />
+    </div>
+  );
+};
+
+export default Filter;
