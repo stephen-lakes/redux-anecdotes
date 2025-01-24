949d4b229621a4a48bd0b2cfbf01ec11390c2a17
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-24 23:37:42 +0100

    Refactor Filter component to dispatch filter change action
    - Imported useDispatch from react-redux
    - Updated handleChange to dispatch filterChange action with the input
      value
    - Now updates search term in the global state via Redux

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,6 +1,12 @@
+import { useDispatch } from "react-redux";
+import { filterChange } from "../reducers/filterReducer";
+
 const Filter = () => {
+  const dispatch = useDispatch();
+
   const handleChange = (event) => {
     event.preventDefault();
+    dispatch(filterChange(event.target.value));
   };
 
   const style = {
