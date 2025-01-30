120858018cac14744f966e6ba517f0e937131586
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-30 14:05:28 +0100

    implement actions creators for notificationSlice

--- src/reducers/notificationReducer.js
+++ src/reducers/notificationReducer.js
@@ -5,7 +5,18 @@ const initialState = "";
 const notificationSlice = createSlice({
   name: "notification",
   initialState,
-  reducers: {},
+  reducers: {
+    showNotification(state, action) {
+      return action.payload;
+    },
+    hideNotification(state, action) {
+      return action.payload;
+    },
+  },
 });
 
+// ecport the actinon creators
+export const { showNotification, hideNotification } = notificationSlice.actions;
+
+// expoet the reducer
 export default notificationSlice.reducer;
