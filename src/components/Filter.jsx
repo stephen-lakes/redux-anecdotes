b532e41162460ebad810acb57362248f438baf44
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-30 13:21:01 +0100

    dispatch the right actions creator

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,12 +1,12 @@
 import { useDispatch } from "react-redux";
-import { filterChange } from "../reducers/filterReducer";
+import {  setFilter } from "../reducers/filterReducer";
 
 const Filter = () => {
   const dispatch = useDispatch();
 
   const handleChange = (event) => {
     event.preventDefault();
-    dispatch(filterChange(event.target.value));
+    dispatch(setFilter(event.target.value));
   };
 
   const style = {
