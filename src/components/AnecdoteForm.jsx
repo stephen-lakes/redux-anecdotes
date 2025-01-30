76a941fef09c948ec1dde6efa054817795aa48e7
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-01-30 14:05:37 +0100

    feat: add notification feature to AnecdoteForm component
    - Imported `useDispatch` from "react-redux" to dispatch actions.
    - Imported `createAnecdote` from "../reducers/anecdoteReducer".
    - Imported `showNotification` and `hideNotification` from
      "../reducers/notificationReducer".
    - Implemented the `addAnecdote` function to handle form submission,
        create a new anecdote, and dispatch a notification.
    - Added a `setTimeout` function to hide the notification after 5
          seconds.

--- src/components/AnecdoteForm.jsx
+++ src/components/AnecdoteForm.jsx
@@ -1,5 +1,6 @@
 import { useDispatch } from "react-redux";
 import { createAnecdote } from "../reducers/anecdoteReducer";
+import { hideNotification, showNotification } from "../reducers/notificationReducer";
 
 const AnecdoteForm = () => {
   const dispatch = useDispatch();
@@ -9,6 +10,9 @@ const AnecdoteForm = () => {
     const content = event.target.anecdote.value;
     dispatch(createAnecdote(content));
     event.target.anecdote.value = "";
+    dispatch(showNotification("A new anecdote was created"));
+    
+    setTimeout(()=> {dispatch(hideNotification(""))}, 5000)
   };
 
   return (
