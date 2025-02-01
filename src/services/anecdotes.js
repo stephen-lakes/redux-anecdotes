ff7a46e2b927f18d8b43a8e3581aca6fe434da52
stephen-lakes <oluyomiolamilekan99@gmail.com>
2025-02-02 00:02:11 +0100

    add a new service function to create a new anecdote

--- src/services/anecdotes.js
+++ src/services/anecdotes.js
@@ -7,4 +7,10 @@ const getAll = async () => {
   return response.data;
 };
 
-export default { getAll };
+const createNewAnecdote = async (content) => {
+  const payload = { content, votes: 0 };
+  const response = await axios.post(baseUrl, payload);
+  return response.data;
+};
+
+export default { getAll, createNewAnecdote };
